51d07847d5ca61d4fe61cb1222b6d8334c2c4635
MojitoBar <wnehdtjr5@gmail.com>
2022-04-13 00:40:06 +0900

    feat: ingame score ui

--- SSC.swiftpm/GameSceneUI.swift
+++ SSC.swiftpm/GameSceneUI.swift
@@ -10,6 +10,20 @@ import GameplayKit
 
 extension GameScene {
     func initLabels() {
+        scoreLabel = SKLabelNode()
+        scoreLabel?.name = "lbScore"
+        scoreLabel?.text = "SCORE: 0"
+        scoreLabel?.fontSize = 18
+        scoreLabel?.fontName = "Courier-Bold"
+        scoreLabel?.horizontalAlignmentMode = .right
+        scoreLabel?.fontColor = .white
+        scoreLabel?.position = CGPoint(x: 350, y: frame.size.height - 80)
+        scoreLabel?.zPosition = 20
         
+        scoreLabel?.run(SKAction.repeatForever(SKAction.sequence([SKAction.run { [self] in
+            score += 10
+            scoreLabel!.text = "SCORE: \(score)"
+        }, SKAction.wait(forDuration: 0.5)])))
+        addChild(scoreLabel!)
     }
 }
