bf28dbee1e8b1ca60d4cbef13500a5f8b69e4162
MojitoBar <wnehdtjr5@naver.com>
2022-04-12 22:55:44 +0900

    feat: coin system

--- SSC.swiftpm/Coin.swift
+++ SSC.swiftpm/Coin.swift
@@ -9,10 +9,34 @@ import Foundation
 import GameplayKit
 
 extension GameScene {
+    func coinRandom(n: Int) -> UInt32 {
+        while true {
+            let temp = arc4random_uniform(9)
+            if temp != n {
+                return temp
+            }
+        }
+    }
     
-    func createCoin() {
-        let position: [(CGFloat, CGFloat)] = [
-            (frame.size.width / 2, frame.size.height / 2),
-        ]
+    func createCoin(index: Int) {
+        let posIndex = coinRandom(n: index)
+        coinIndex = Int(posIndex)
+        
+        coin.size = CGSize(width: 25, height: 25)
+        coin.position = CGPoint(x: coinPosition![Int(posIndex)].0, y: coinPosition![Int(posIndex)].1)
+        
+        coin.physicsBody = SKPhysicsBody(rectangleOf: coin.size)
+        coin.physicsBody?.isDynamic = true
+        coin.physicsBody?.categoryBitMask = PhysicsCategory.coin
+        coin.physicsBody?.contactTestBitMask = PhysicsCategory.Hero
+        coin.physicsBody?.collisionBitMask = PhysicsCategory.None
+        
+        addChild(coin)
+    }
+    
+    func changeCoinPosition(index: Int) {
+        let posIndex = coinRandom(n: index)
+        coinIndex = Int(posIndex)
+        coin.run(SKAction.move(to: CGPoint(x: coinPosition![Int(posIndex)].0, y: coinPosition![Int(posIndex)].1), duration: 0))
     }
 }
