8caedf3f94f85295e46b2c5e236dd69579bb2d07
MojitoBar <wnehdtjr5@naver.com>
2022-04-12 22:22:09 +0900

    feat[#4]: change image resources

---
+++ SSC.swiftpm/Coin.swift
@@ -0,0 +1,18 @@
+//
+//  Coin.swift
+//  SSC
+//
+//  Created by judongseok on 2022/04/12.
+//
+
+import Foundation
+import GameplayKit
+
+extension GameScene {
+    
+    func createCoin() {
+        let position: [(CGFloat, CGFloat)] = [
+            (frame.size.width / 2, frame.size.height / 2),
+        ]
+    }
+}
--- SSC.swiftpm/GameSceneUI.swift
+++ SSC.swiftpm/GameSceneUI.swift
@@ -10,21 +10,6 @@ import GameplayKit
 
 extension GameScene {
     func initLabels() {
-        lblScore.name = "lblScore"
-        lblScore.text = "Score: "
-        lblScore.fontSize = 64
-        lblScore.fontColor = .red
-        lblScore.position = CGPoint(x: 200, y: 200)
-        lblScore.zPosition = 11
-        addChild(lblScore)
-        
-        lblTitle.name = "lblTitle"
-        lblTitle.text = "My Game"
-        lblTitle.fontSize = 64
-        lblTitle.fontColor = .red
-        lblTitle.position = CGPoint(x: 500, y: 500)
-        lblTitle.zPosition = 11
-        addChild(lblTitle)
         
     }
 }
